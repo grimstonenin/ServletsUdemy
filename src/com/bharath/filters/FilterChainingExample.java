f5e7c536eb923f184d9f05e96e4fa66e2b990909
Andrei Cristea <andreicristea@Andreis-MacBook-Pro.local>
2018-02-14 14:01:42 +0200

    Filters and filter chaining example

--- src/com/bharath/filters/FilterChainingExample.java
+++ src/com/bharath/filters/FilterChainingExample.java
@@ -5,8 +5,8 @@ import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebFilter(filterName = "filter1",urlPatterns = {"/FilterServletDemo"})
-public class FilterDemo implements Filter{
+//@WebFilter(filterName = "filter2",urlPatterns = {"/FilterServletDemo"})
+public class FilterChainingExample implements Filter{
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -16,9 +16,9 @@ public class FilterDemo implements Filter{
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         PrintWriter out = servletResponse.getWriter();
-        out.println("pre servlet - first filter");
+        out.println("pre servlet - second filter");
         filterChain.doFilter(servletRequest,servletResponse);
-        out.println("post servlet - first filter");
+        out.println("post servlet - second filter");
     }
 
     @Override
